44289eb78bef8b469be5b141bc6baac41d01494a
Alex Komendant <komendantaa@gmail.com>
2022-01-11 16:14:25 +0200

    templates uploadert

---
+++ src/scripts/templates.js
@@ -0,0 +1,12 @@
+Array.from(document.querySelectorAll("[data-template]")).forEach(elem => {
+    var xhr= new XMLHttpRequest();
+    xhr.open('GET', elem.getAttribute('data-template'), true);
+    xhr.onreadystatechange= function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return;
+        elem.outerHTML = this.responseText;
+    };
+    xhr.send();
+})
+
+
